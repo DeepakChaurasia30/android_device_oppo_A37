e705196ffa18585987e01eb34c1b8631e7021501
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-08-11 16:32:23 +0530

    A37 doesn't have such prop

--- system_prop.mk
+++ system_prop.mk
@@ -21,11 +21,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.target=dpm1 \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.multisim.config=dsds \
     persist.radio.custom_ecc=1 \
     persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
-    ril.subscription.types=RUIM
+    ril.subscription.types=NV,RUIM
