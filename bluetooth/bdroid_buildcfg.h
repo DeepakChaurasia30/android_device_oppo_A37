f3f2950159823d7d6bc0bfed30a719b45c53aefe
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-10-24 14:43:05 +0530

    Bt: update blobs for bt and bdroid config.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,15 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "OPPO A37"
+#define BTM_DEF_LOCAL_NAME   "Oppo A37f"
 #define BLUETOOTH_QTI_SW TRUE
+#define BTC_INCLUDED TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    7
+#define MAX_L2CAP_CHANNELS    16
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_VND_INCLUDED   TRUE
+#define BLE_PERIPHERAL_ADV_NAME  TRUE
 #endif
